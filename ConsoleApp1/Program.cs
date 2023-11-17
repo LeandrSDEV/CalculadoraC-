9f61233d6f0a1a34b4560c876b70a3202dde10db
LEANDRO <LEANDRO@DESKTOP-630KO0H>
2023-11-17 19:25:14 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual operação desseja faazer:");
+            Console.WriteLine("1- Adição");
+            Console.WriteLine("2- Subtração");
+            Console.WriteLine("3- Multiplicação");
+            Console.WriteLine("4- Divisão\n");
+
+            int operacao = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int resultado = 0;
+
+            switch (operacao)
+            {
+                case 1:
+                    {
+                        resultado = Adicao(num1, num2);
+                        break;
+                    }
+                case 2:
+                    {
+                        resultado = Suntracao(num1, num2);
+                        break;
+                    }
+                case 3:
+                    {
+                        resultado = Multiplicacao(num1, num2);
+                        break;
+                    }
+                case 4:
+                    {
+                        resultado = Divisao(num1, num2);
+                        break;
+                    }
+
+                default:
+                    Console.WriteLine("Número invalido, digite outro número.");
+                    break;
+            }
+
+            Console.WriteLine("O resultado da operação com os números {0} é {1} é: {2}", num1, num2, resultado);
+
+            Console.ReadLine();
+        }
+
+        public static int Adicao(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+        }
+        public static int Suntracao(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            return resultado;
+        }
+        public static int Multiplicacao(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            return resultado;
+        }
+        public static int Divisao(int num1, int num2)
+        {
+            int resultado = num1 / num2;
+            return resultado;
+        }
+    }
+}
